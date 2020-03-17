40fcfa3cff10feebfee81ee4907900027098db11
samuel-1978 <62302348+samuel-1978@users.noreply.github.com>
2020-03-18 01:19:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Description: Samuel OpenWRT
+# Lisence: HUHU
+# Author: sAmueL/eSir PlayGroud
+# Youtube Channel: NotYET 
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
